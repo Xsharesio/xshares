c4ddc2a78ea491a62e06e6a0dbfcc611b5d07433
Lamagame@99 <support@xshares.io>
2023-09-02 04:48:42 +0000

    update

--- XShares.sol
+++ XShares.sol
@@ -1,129 +1,9 @@
-/**
- *Submitted for verification at basescan.org on 2023-08-10
-*/
-
-// File: contracts/Context.sol
-
-
-// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-// File: contracts/Ownable.sol
-
-
-// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
-
-pragma solidity ^0.8.0;
-
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-// File: contracts/FriendtechShares.sol
-
-
-
 pragma solidity >=0.8.2 <0.9.0;
 
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// TODO: Events, final pricing model, 
 
-contract XSharesSharesV1 is Ownable { 
+contract XSharesSharesV2 is OwnableUpgradeable {
     address public protocolFeeDestination;
     uint256 public protocolFeePercent;
     uint256 public subjectFeePercent;
@@ -137,7 +17,15 @@ contract XSharesSharesV1 is Ownable {
     mapping(address => uint256) public sharesSupply;
 
     // SharesSubject => CurveIndex
-    mapping(address => uint256) public sharesCurveIndex; 
+    mapping(address => uint256) public sharesCurveIndex;
+
+
+    function initialize() public initializer {
+        __Ownable_init();
+        protocolFeeDestination = 0x803e17c7A84af859F5d85A8Cd75736265a133980;
+        protocolFeePercent = 1e17;
+    }
+
 
     function setFeeDestination(address _feeDestination) public onlyOwner {
         protocolFeeDestination = _feeDestination;
@@ -152,19 +40,20 @@ contract XSharesSharesV1 is Ownable {
     }
 
     function getPrice(uint256 supply, uint256 amount, uint256 curveIndex) public pure returns (uint256) {
-        uint256 sum1 = supply == 0 ? 0 : ((supply - 1 )* (supply) * (2 * (supply - 1) + 1) * 1 ether ) / (curveIndex * 6);
-        uint256 sum2 = supply == 0 && amount == 1 ? 0 : ((supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) * 1 ether )/ (curveIndex * 6); 
+        uint256 sum1 = supply == 0 ? 0 : ((supply - 1) * (supply) * (2 * (supply - 1) + 1) * 1 ether) / (curveIndex * 6);
+        uint256 sum2 = supply == 0 && amount == 1 ? 0 : ((supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) * 1 ether) / (curveIndex * 6);
         uint256 summation = sum2 - sum1;
         // ((x ** 2 - 1) / p) + 1 / 16000
-        return summation / 16000;   
+        return summation / 16000;
     }
+
 
     function getBuyPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
         return getPrice(sharesSupply[sharesSubject], amount, sharesCurveIndex[sharesSubject]);
     }
 
     function getSellPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
-        return getPrice(sharesSupply[sharesSubject] - amount, amount,sharesCurveIndex[sharesSubject]); 
+        return getPrice(sharesSupply[sharesSubject] - amount, amount, sharesCurveIndex[sharesSubject]);
     }
 
     function getBuyPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
@@ -183,49 +72,47 @@ contract XSharesSharesV1 is Ownable {
 
     function buyShares(address sharesSubject, uint256 amount) public payable {
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply > 0 , "Users are not registered and cannot purchase"); 
-        uint256 price = getPrice(supply, amount,sharesCurveIndex[sharesSubject]);
+        require(supply > 0, "Users are not registered and cannot purchase");
+        uint256 price = getPrice(supply, amount, sharesCurveIndex[sharesSubject]);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
         uint256 subjectFee = price * subjectFeePercent / 1 ether;
         require(msg.value >= price + protocolFee + subjectFee, "Insufficient payment");
         sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] + amount;
         sharesSupply[sharesSubject] = supply + amount;
         emit Trade(msg.sender, sharesSubject, true, amount, price, protocolFee, subjectFee, supply + amount);
-        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
+        (bool success1,) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success2,) = sharesSubject.call{value: subjectFee}("");
         require(success1 && success2, "Unable to send funds");
     }
 
-    function sellShares(address sharesSubject, uint256 amount) public payable {
+    function sellShares(address sharesSubject, uint256 amount) public {
         uint256 supply = sharesSupply[sharesSubject];
         require(supply > amount, "Cannot sell the last share");
-        uint256 price = getPrice(supply - amount, amount,sharesCurveIndex[sharesSubject]);
+        uint256 price = getPrice(supply - amount, amount, sharesCurveIndex[sharesSubject]);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
         uint256 subjectFee = price * subjectFeePercent / 1 ether;
         require(sharesBalance[sharesSubject][msg.sender] >= amount, "Insufficient shares");
         sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] - amount;
         sharesSupply[sharesSubject] = supply - amount;
         emit Trade(msg.sender, sharesSubject, false, amount, price, protocolFee, subjectFee, supply - amount);
-        (bool success1, ) = msg.sender.call{value: price - protocolFee - subjectFee}("");
-        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success3, ) = sharesSubject.call{value: subjectFee}("");
+        (bool success1,) = msg.sender.call{value: price - protocolFee - subjectFee}("");
+        (bool success2,) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success3,) = sharesSubject.call{value: subjectFee}("");
         require(success1 && success2 && success3, "Unable to send funds");
     }
 
-    function register(address sharesSubject,uint256 curveIndex) public payable{
+    function register(uint256 curveIndex) public  {
+        address sharesSubject = msg.sender;
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply == 0 || sharesSubject == msg.sender, "Only the shares' subject can register");  
-        require(curveIndex >= 1 && curveIndex <= 10,"The curveIndex can only be between 1 and 10");
-        uint256 price = getPrice(supply, 1 ,curveIndex);  
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        require(msg.value >= price + protocolFee + subjectFee, "Insufficient payment");
-        sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] + 1; 
+        require(supply == 0 , "Supply must be 0");
+        require(curveIndex >= 1 && curveIndex <= 10, "The curveIndex can only be between 1 and 10");
+        sharesBalance[sharesSubject][sharesSubject] = sharesBalance[sharesSubject][sharesSubject] + 1;
         sharesSupply[sharesSubject] = supply + 1;
-        sharesCurveIndex[sharesSubject] = curveIndex; 
-        emit Trade(msg.sender, sharesSubject, true, 1, price, protocolFee, subjectFee, supply + 1);
-        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
-        require(success1 && success2, "Unable to send funds");
+        sharesCurveIndex[sharesSubject] = curveIndex;
+        emit Trade(sharesSubject, sharesSubject, true, 1, 0, 0, 0, supply + 1);
     }
+
+
+
+
 }
